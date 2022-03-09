e48b3195d3839909c123d32767d4db2eefc40f41
Anders Jonsson <aj224rj@student.lnu.se>
2022-03-09 11:41:09 +0100

    Install external modules

--- src/routes/images-router.js
+++ src/routes/images-router.js
@@ -13,7 +13,7 @@ export const router = express.Router()
 const controller = new ResourceController()
 
 // Get all images.
-router.get('/', /*authenticate*/ /*authorize*/ (req, res, next) => controller.index(req, res, next))
+router.get('/', /* authenticate */ /* authorize */ (req, res, next) => controller.index(req, res, next))
 
 // Upload image.
-router.post('/', /*authenticate*/ (req, res, next) => controller.register(req, res, next))
+router.post('/', /* authenticate */ (req, res, next) => controller.register(req, res, next))
