0e1db933f8e13699bcb8e8facddcb361a59358f9
Anders Jonsson <aj224rj@student.lnu.se>
2022-03-13 23:03:30 +0100

    Importing documentation from file

--- src/routes/router.js
+++ src/routes/router.js
@@ -6,12 +6,35 @@
  */
 
 import express from 'express'
+import { readFile } from 'fs/promises'
 import createError from 'http-errors'
 import { router as imageRouter } from './images-router.js'
 
+/**
+ * Read from json-file.
+ *
+ * @param {string} path - The realative path to json-document.
+ * @returns {object} - Documentation object.
+ */
+const getDocumentation = async (path) => {
+  let docs
+  try {
+    const textFromFile = await readFile(path, 'utf8')
+    docs = JSON.parse(textFromFile, 2)
+  } catch (error) {
+    docs = {
+      message: 'Welcome to the resource-service! Use the /images endpoint to handle resources.'
+    }
+  }
+
+  return docs
+}
+
+const documentation = await getDocumentation('./data/docs.json')
+
 export const router = express.Router()
 
-router.get('/', (req, res) => res.json({ message: 'Welcome to the resource-service! Use the /images endpoint to handle resources.' }))
+router.get('/', (req, res) => res.json(documentation))
 router.use('/images', imageRouter)
 
 // Catch 404 (ALWAYS keep this as the last route).
