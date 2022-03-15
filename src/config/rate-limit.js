523d0a5f29a3ab331e973affcfedfe0d7f485171
Anders Jonsson <aj224rj@student.lnu.se>
2022-03-15 11:01:28 +0100

    Adjust comment

--- src/config/rate-limit.js
+++ src/config/rate-limit.js
@@ -9,7 +9,7 @@ import rateLimit from 'express-rate-limit'
 
 export const rateLimiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 10 minutes)
   standardHeaders: false, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: true, // Disable the `X-RateLimit-*` headers
   message: 'Exceeded request limit, please try again later.'
