d5e9f76cf8017d2c132f40c6142bb5979317011f
Anders Jonsson <aj224rj@student.lnu.se>
2022-03-16 16:32:21 +0100

    Replace file-reading with import of documentation-object

--- src/routes/router.js
+++ src/routes/router.js
@@ -6,31 +6,9 @@
  */
 
 import express from 'express'
-import { readFile } from 'fs/promises'
 import createError from 'http-errors'
 import { router as imageRouter } from './images-router.js'
-
-/**
- * Read from json-file.
- *
- * @param {string} path - The realative path to json-document.
- * @returns {object} - Documentation object.
- */
-const getDocumentation = async (path) => {
-  let docs
-  try {
-    const textFromFile = await readFile(path, 'utf8')
-    docs = JSON.parse(textFromFile, 2)
-  } catch (error) {
-    docs = {
-      message: 'Welcome to the resource-service! Use the /images endpoint to handle resources.'
-    }
-  }
-
-  return docs
-}
-
-const documentation = await getDocumentation('./data/docs.json')
+import { documentation } from '../documentation/documentation.js'
 
 export const router = express.Router()
 
